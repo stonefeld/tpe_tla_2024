7a9863a3df5bf459cdd6e7265ae573b089816f15
PakiQuian <f.quianblanco@gmail.com>
2024-06-20 01:19:31 -0300

    Cambio en el build.bat para poder compilar con md

--- script/windows/build.bat
+++ script/windows/build.bat
@@ -4,6 +4,8 @@
 @set BASE_PATH=%BASE_PATH:~0,-1%
 @set BASE_PATH=%BASE_PATH%\..\..
 
+if "%~1" == "--md" @set MARKDOWN=-DMARKDOWN_GENERATOR=1
+
 @echo [93mBuilding the compiler...[0m
 @echo:
 
@@ -12,7 +14,7 @@ del %BASE_PATH%\src\main\c\frontend\lexical-analysis\FlexScanner.c >nul 2>&1
 del %BASE_PATH%\src\main\c\frontend\syntactic-analysis\BisonParser.c >nul 2>&1
 del %BASE_PATH%\src\main\c\frontend\syntactic-analysis\BisonParser.h >nul 2>&1
 
-cmake -S %BASE_PATH% -B %BASE_PATH%\build
+cmake %MARKDOWN% -S %BASE_PATH% -B %BASE_PATH%\build
 
 @echo:
 @echo [93mCMake finished scaffolding. Find the project inside 'build' folder.[0m
